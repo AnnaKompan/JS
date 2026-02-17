b0acd3fe301331e92f464bab10144a72d03e7369
Anna Kompan <annakompanch@gmail.com>
2026-02-17 23:59:07 +0200

    promises

---
+++ async_await.js
@@ -0,0 +1,6 @@
+/**
+ * async/await is allows write asynchronous code in a synchronous way.
+ *
+ * async - makes function return a promise
+ * await - makes function wait for a promise
+ */
---
+++ promises.js
@@ -0,0 +1,71 @@
+/**
+ * Promise - obj, that manages asynchronous operations and their results (fetch data, perform tasks, etc.)
+ * - wrap promise around any asynchronous operation,
+ * and it will show when operation completes or fails (Pending -> Resolved, Rejected)
+ * - with promise we don't need callbacks, we can use .then() and .catch() to handle results and errors
+ *
+ * new Promise((resolve, reject) => {
+ *   // do something asynchronous
+ *   if (success) {
+ *     resolve(result);
+ *   } else {
+ *     reject(error);
+ *   }
+ * });
+ */
+
+// Do chores in order(1. walk dog, 2. clean kitchen, 3. take out trash)
+
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    const dogWalked = true;
+    setTimeout(() => {
+      if (dogWalked) {
+        // make asynchronous by setting timer
+        resolve("You walked the dog");
+        // console.log("You walked the dog");
+        // callback();
+      } else {
+        reject("You didn't walk the dog");
+      }
+    }, 1600);
+  });
+}
+
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You cleaned kitchen");
+    }, 2500);
+  });
+}
+
+function takeTrash() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You took all the trash");
+    }, 800);
+  });
+}
+
+// walkDog(() => {
+//   cleanKitchen(() => {
+//     takeTrash(() => {
+//       console.log("You finished all the chores");
+//     });
+//   });
+// });
+
+// Method chaining
+walkDog()
+  .then((value) => {
+    console.log(value);
+    return cleanKitchen();
+  })
+  .then((value) => {
+    console.log(value);
+    return takeTrash().then((value) => {
+      console.log(value);
+      console.log("You finished all the chores");
+    });
+  });

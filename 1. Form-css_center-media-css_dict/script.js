77a64c05ea3df8c1768a870c395879fdd1ecfc6a
Anna Kompan <annakompanch@gmail.com>
2026-04-01 18:05:01 +0800

    4+5 center @media :root

--- 1. Form-css_center-media-css_dict/script.js
+++ 1. Form-css_center-media-css_dict/script.js
@@ -1,11 +1,10 @@
 const form = document.getElementById("loginForm");
-const message = document.getElementById("message");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const email = form.elements.email.value;
-  const password = form.elements.password.value;
-  console.log(email, password);
-  message.textContent = `Email: ${email}, Password: ${password}`;
+  const { email, phone, password } = e.target.elements;
+  console.log(
+    `Email: ${email.value} Phone: ${phone.value} Password: ${password.value}`,
+  );
 });

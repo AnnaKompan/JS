055b7607de583cf2b46f450eb89f6bf3dc6f89c8
Anna Kompan <annakompanch@gmail.com>
2026-04-08 23:11:35 +0800

    Modal popup

--- 1. Form-css_center-media-css_dict/script.js
+++ 1. Form-css_center-media-css_dict/script.js
@@ -1,10 +1,37 @@
-const form = document.getElementById("loginForm");
+// const form = document.getElementById("loginForm");
+const openBtn = document.getElementById("modal-popup");
+const closeBtn = document.getElementById("close-btn");
+const backdrop = document.querySelector(".backdrop");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
 
-  const { email, phone, password } = e.target.elements;
-  console.log(
-    `Email: ${email.value} Phone: ${phone.value} Password: ${password.value}`,
-  );
+//   const { email, phone, password } = e.target.elements;
+//   console.log(
+//     `Email: ${email.value} Phone: ${phone.value} Password: ${password.value}`,
+//   );
+// });
+
+function openModal() {
+  backdrop.classList.remove("is-hidden");
+}
+
+function closeModal() {
+  backdrop.classList.add("is-hidden");
+}
+openBtn.addEventListener("click", openModal);
+
+closeBtn.addEventListener("click", closeModal);
+
+backdrop.addEventListener("click", (e) => {
+  if (e.target === backdrop) {
+    closeModal();
+  }
+});
+
+// close по ESC
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeModal();
+  }
 });

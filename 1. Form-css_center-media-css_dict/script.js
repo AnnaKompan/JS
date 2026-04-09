6c4665cb2d81ce38fadc62540a464dc8339209ed
Anna Kompan <annakompanch@gmail.com>
2026-04-10 01:09:03 +0800

    modal form

--- 1. Form-css_center-media-css_dict/script.js
+++ 1. Form-css_center-media-css_dict/script.js
@@ -1,6 +1,6 @@
 // const form = document.getElementById("loginForm");
-const openBtn = document.getElementById("modal-popup");
-const closeBtn = document.getElementById("close-btn");
+const openBtn = document.querySelector(".open-modal");
+const closeBtn = document.querySelector(".close-btn");
 const backdrop = document.querySelector(".backdrop");
 
 // form.addEventListener("submit", function (e) {
@@ -12,26 +12,15 @@ const backdrop = document.querySelector(".backdrop");
 //   );
 // });
 
-function openModal() {
+openBtn.addEventListener("click", () => {
   backdrop.classList.remove("is-hidden");
-}
-
-function closeModal() {
+});
+closeBtn.addEventListener("click", () => {
   backdrop.classList.add("is-hidden");
-}
-openBtn.addEventListener("click", openModal);
-
-closeBtn.addEventListener("click", closeModal);
-
-backdrop.addEventListener("click", (e) => {
-  if (e.target === backdrop) {
-    closeModal();
-  }
 });
 
-// close по ESC
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
-    closeModal();
+    backdrop.classList.add("is-hidden");
   }
 });

2473494b96dbd03173d5b577ca403d2c4951a265
Anna Kompan <annakompanch@gmail.com>
2026-04-19 16:12:46 +0800

    SCSS for formtest

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,16 +1,13 @@
-const form = document.querySelector(".form");
-const formInput = document.querySelector(".form-txt");
+const form = document.querySelector("form");
+const formInput = document.querySelector("form__text");
 const localStorageKey = "form-text";
-const delBtn = document.querySelector(".form-del-btn");
+const delBtn = document.querySelector("form__btn--delete");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  localStorage.setItem(localStorageKey, e.target.elements.text.value);
+  if (e.submitter.classList.contains("form__btn")) {
+    localStorage.setItem("test", e.target.elements.text.value);
+  } else {
+    localStorage.removeItem("test");
+  }
 });
-delBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  localStorage.removeItem(localStorageKey);
-  form.reset();
-});
-
-formInput.value = localStorage.getItem(localStorageKey) ?? "";
